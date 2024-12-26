f65267f9744247ea2a6263579ecd2ab6dcb2ce06
Ingrid K Vega Ordoñez <ingridkvegao@gmail.com>
2024-12-25 21:35:54 -0500

    Add files via upload

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+function Header (){
+    return (
+        
+    <header className="encabezado">
+        <img src="/img/cabecera.png" alt="encabezado" />
+        </header>
+   
+)
+}
+export default Header
